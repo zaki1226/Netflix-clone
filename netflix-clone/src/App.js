ff792960972b2842b90f68540e1a2baf8d680bd6
Zak Man <zakm01163.com>
2025-07-28 19:20:07 +0300

    row

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 import Home from './pages/Home/Home';
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home />
-      
-  
-      
     </div>
   );
 }
